454e66e4b111d2a97b619e458eb8b7df8c02af81
Hydra <94378325+OxyHydra@users.noreply.github.com>
2022-07-01 22:09:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Discord Bot Developer", "Web Developer", "Discord Server Developer", "Discord Server Manager", " ", " "],
+        strings: ["Discord Bot Developer", "Web Designer", "Discord Server Developer", "Discord Community Manager", " ", " "],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Discord Bot Developer", "Web Developer", "Discord Server Developer", "Discord Server Manager", " ", ""],
+        strings: ["Discord Bot Developer", "Web Designer", "Discord Server Developer", "Discord Community Manager", " ", ""],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
